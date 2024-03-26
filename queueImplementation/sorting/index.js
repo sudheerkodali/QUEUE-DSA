521b91ff259b86e77e3be204cf980f73f90952f8
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2024-03-26 15:00:41 +0530

    Update index.js

--- queueImplementation/sorting/index.js
+++ queueImplementation/sorting/index.js
@@ -1,1 +1,15 @@
+const Queue = require('./queue/Queue');
+
+q = new Queue();
+
+q.add(5);
+q.add(4);
+q.add(6);
+
+
+while (!q.isEmpty()) {
+  console.log('Front element of the queue is ${q.peek()}');
+  q.remove();
+
+}
 
